e087de797d18f69b6eca7f49c6db99716b03eb8a
Durai Pandian <dduraipandian@gmail.com>
2026-01-06 15:57:22 +0530

    moving flow into a new app

--- components/flow/constants.js
+++
@@ -1,9 +0,0 @@
-export const NODE_MOVED_EVENT = "node:moved";
-export const NODE_DROPPED_EVENT = "node:dropped";
-export const NODE_REMOVED_EVENT = "node:removed";
-
-// Connection Events
-export const CONNECTION_CREATED_EVENT = "connection:created";
-export const CONNECTION_REMOVED_EVENT = "connection:removed";
-export const CONNECTION_UPDATED_EVENT = "connection:updated";
-export const CONNECTION_CLICKED_EVENT = "connection:clicked";
