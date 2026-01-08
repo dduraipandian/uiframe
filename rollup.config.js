f24cfbf62f8bdccfc1bb97a4d8f9b0ffeff98d16
Durai Pandian <dduraipandian@gmail.com>
2026-01-08 13:36:58 +0530

    basic setup for packaging and adding core package

---
+++ rollup.config.js
@@ -0,0 +1,57 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import { terser } from "rollup-plugin-terser";
+import path from "path";
+import fs from "fs";
+
+const packagesDir = path.resolve(__dirname, "packages");
+const packages = fs.readdirSync(packagesDir).filter((pkg) => {
+    return fs.statSync(path.join(packagesDir, pkg)).isDirectory();
+});
+
+function capitalize(name) {
+    return name.charAt(0).toUpperCase() + name.slice(1);
+}
+
+export default packages.map((pkg) => {
+    const pkgPath = path.join(packagesDir, pkg);
+    const pkgJson = JSON.parse(
+        fs.readFileSync(path.join(pkgPath, "package.json"), "utf-8")
+    );
+
+    return {
+        input: path.join(pkgPath, "index.js"),
+        output: [
+            {
+                file: path.join(pkgPath, "dist/index.js"),
+                format: "cjs",
+                sourcemap: true,
+            },
+            {
+                file: path.join(pkgPath, "dist/index.esm.js"),
+                format: "esm",
+                sourcemap: true,
+            },
+            {
+                file: path.join(pkgPath, `dist/${pkg}.min.js`),
+                format: "iife",
+                name: pkg === "core" ? "UIFrameCore" : `UIFrame${capitalize(pkg)}`,
+                plugins: [terser()],
+                globals: (id) => {
+                    if (id.startsWith("@uiframe/")) {
+                        const name = id.split("/")[1];
+                        return name === "core" ? "UIFrameCore" : `UIFrame${capitalize(name)}`;
+                    }
+                }
+            },
+        ],
+        external: [
+            ...Object.keys(pkgJson.peerDependencies || {}),
+            (id) => id.startsWith("@uiframe/") && id !== pkgJson.name,
+        ],
+        plugins: [
+            resolve(),
+            commonjs(),
+        ],
+    };
+});
