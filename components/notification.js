5daf6378c4bed6e7bf6fc1652951d6dcc1ebe6ef
Durai Pandian <dduraipandian@gmail.com>
2026-01-11 19:02:34 +0530

    improving notification and online component

--- components/notification.js
+++ components/notification.js
@@ -5,12 +5,33 @@ class Notification extends EmitterComponent {
     super({ name });
 
     this.options = options;
+    this.autoHideOnError = options.autohide_on_error || undefined;
 
     this.INFO = "info";
     this.SUCCESS = "success";
     this.ERROR = "error";
     this.WARNING = "warning";
 
+    this.disapearAfter = options.disappear_after || 3000;
+    this.notificationContainer = null;
+    this.querySelector = "#uiframe-notification-container .toast-container";
+    this.createNotificationContainer();
+  }
+
+  createNotificationContainer() {
+    const container = document.querySelector(this.querySelector);
+    if (!container) {
+      const notificationContainerHtml = `
+        <div id="uiframe-notification-container" aria-live="polite" aria-atomic="true" class="uiframe-notification-container position-relative">
+            <div class="toast-container bottom-0 end-0"></div>
+        </div>
+      `;
+      document.body.innerHTML += notificationContainerHtml;
+    }
+
+    this.notificationContainer = container ?? document.querySelector(this.querySelector);
+
+    console.log(this.notificationContainer);
     this.#getDiv(this.INFO);
     this.#getDiv(this.SUCCESS);
     this.#getDiv(this.ERROR);
@@ -18,36 +39,34 @@ class Notification extends EmitterComponent {
   }
 
   #getDiv(level) {
-    const div = document.createElement("div");
-    div.innerHTML = this.html(level);
-    document.body.appendChild(div);
-    return div;
+    const htm = this.html(level);
+    this.notificationContainer.insertAdjacentHTML("beforeend", htm);
   }
 
   #errorModal() {
     return `
-            <div class="modal fade" id="errorNotificationModal" 
-                data-bs-backdrop="static" 
-                data-bs-keyboard="false" 
-                tabindex="-1" 
-                aria-labelledby="errorNotificationModalTitle" 
-                aria-hidden="true">
-                <div class="modal-dialog modal-dialog-centered">
-                    <div class="modal-content" style="max-width: 650px; width: 650px">
-                        <div class="modal-header">
-                            <h1 class="modal-title fs-5" id="errorNotificationModalTitle"></h1>
-                            <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <div class="modal-body">
-                            <pre id="errorNotificationModalMessage"></pre>
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        `;
+      <div class="modal fade" id="${this.id}-errorNotificationModal" 
+          data-bs-backdrop="static" 
+          data-bs-keyboard="false" 
+          tabindex="-1" 
+          aria-labelledby="${this.id}-errorNotificationModalTitle" 
+          aria-hidden="true">
+          <div class="modal-dialog modal-dialog-centered">
+              <div class="modal-content" style="max-width: 650px; width: 650px">
+                  <div class="modal-header">
+                      <h1 class="modal-title fs-5" id="${this.id}-errorNotificationModalTitle"></h1>
+                      <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
+                  </div>
+                  <div class="modal-body">
+                      <pre id="${this.id}-errorNotificationModalMessage"></pre>
+                  </div>
+                  <div class="modal-footer">
+                      <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                  </div>
+              </div>
+          </div>
+      </div>
+    `;
   }
 
   html(level) {
@@ -60,37 +79,36 @@ class Notification extends EmitterComponent {
     } else if (level === "error") {
       bgClass = "bg-danger text-white";
       modalHtml = this.#errorModal();
-      autoHide = false;
+      autoHide = this.autoHideOnError === undefined ? false : this.autoHideOnError;
     } else if (level === "warning") {
       bgClass = "bg-warning text-dark";
     }
 
     return `
-            ${modalHtml}
-            <div class="uiframe-notification-container position-fixed bottom-0 end-0 p-3 me-2 mb-3">
-                <div id="notification-${level}" 
-                    class="toast align-items-center ${bgClass}" 
-                    role="alert"
-                    aria-live="assertive"
-                    data-bs-autohide="${autoHide}"
-                    aria-atomic="true" 
-                    data-bs-delay="3000">
-                    <div class="d-flex">
-                        <div id="toast-body-${level}" class="toast-body"></div>
-                        <button type="button" 
-                            class="btn-close btn-close-white me-2 m-auto" 
-                            data-bs-dismiss="toast" 
-                            aria-label="Close">
-                        </button>
-                    </div>
+            ${modalHtml}            
+            <div id="${this.id}-notification-${level}" 
+                class="toast align-items-center ${bgClass}" 
+                role="alert"
+                aria-live="assertive"
+                data-bs-autohide="${autoHide}"
+                aria-atomic="true" 
+                data-bs-delay="${this.disapearAfter}">
+                <div class="d-flex">
+                    <div id="${this.id}-toast-body-${level}" class="toast-body"></div>
+                    <button type="button" 
+                        class="btn-close btn-close-white me-2 m-auto" 
+                        data-bs-dismiss="toast" 
+                        aria-label="Close">
+                    </button>
                 </div>
             </div>`;
   }
+
   #showToast(message, level = "info") {
     console.debug("Showing toast message:", message);
 
-    let toastElement = document.getElementById(`notification-${level}`);
-    const toastBody = document.getElementById(`toast-body-${level}`);
+    let toastElement = document.getElementById(`${this.id}-notification-${level}`);
+    const toastBody = document.getElementById(`${this.id}-toast-body-${level}`);
     toastBody.innerHTML = message;
 
     const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastElement);
@@ -99,15 +117,15 @@ class Notification extends EmitterComponent {
 
   #showErrorModal(title, message) {
     console.error("Showing error modal:", message);
-    const modalTitle = document.getElementById("errorNotificationModalTitle");
-    const modalBody = document.getElementById("errorNotificationModalMessage");
+    const modalTitle = document.getElementById(`${this.id}-errorNotificationModalTitle`);
+    const modalBody = document.getElementById(`${this.id}-errorNotificationModalMessage`);
 
-    const toastBody = document.getElementById("toast-body-error");
+    const toastBody = document.getElementById(`${this.id}-toast-body-error`);
 
     toastBody.innerHTML += `
             <span type="button"
                 data-bs-toggle="modal"       
-                data-bs-target="#errorNotificationModal"> 
+                data-bs-target="#${this.id}-errorNotificationModal"> 
                     <u>show</u>
             </span>`;
 
