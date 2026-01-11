20276c39a6a216c1f0929e4b6249c8473836c830
dduraipandian <44367425+dduraipandian@users.noreply.github.com>
2026-01-11 17:42:42 +0000

    Deploying to gh-pages from @ dduraipandian/uiframe@fbca0275b56cad816d81440a7477e7e51ce01269 🚀

--- packages/notification/index.js
+++ packages/notification/index.js
@@ -2,23 +2,15 @@ import "../../components/css/notification.css";
 
 import { Notification } from "../../components/notification.js";
 
-let instance = null;
+let instance = new WeakMap();
 
-function getInstance({ app, options = {} }) {
-  // IIFE / Browser path
-  if (typeof window !== "undefined" && window.uiframe) {
-    window.uiframe._notification =
-      window.uiframe._notification || new Notification({ name: app, options });
-
-    return window.uiframe._notification;
+function getInstance(params) {
+  // const params = { name, options = {} };
+  if (!instance.has(params)) {
+    instance.set(params, new Notification(params));
   }
 
-  // ESM / CJS path
-  if (!instance) {
-    instance = new Notification({ name: app, options });
-  }
-
-  return instance;
+  return instance.get(params);
 }
 
 export { Notification, getInstance as notification };
--- packages/online/dist/index.js
+++ packages/online/dist/index.js
@@ -4,6 +4,162 @@ Object.defineProperty(exports, '__esModule', { value: true });
 
 var core = require('@uiframe/core');
 
+class Notification extends core.EmitterComponent {
+  constructor({ name, options = {} }) {
+    super({ name });
+
+    this.options = options;
+    this.autoHideOnError = options.autohide_on_error || undefined;
+
+    this.INFO = "info";
+    this.SUCCESS = "success";
+    this.ERROR = "error";
+    this.WARNING = "warning";
+
+    this.disapearAfter = options.disappear_after || 3000;
+    this.notificationContainer = null;
+    this.querySelector = "#uiframe-notification-container .toast-container";
+    this.createNotificationContainer();
+  }
+
+  createNotificationContainer() {
+    const container = document.querySelector(this.querySelector);
+    if (!container) {
+      const notificationContainerHtml = `
+        <div id="uiframe-notification-container" aria-live="polite" aria-atomic="true" class="uiframe-notification-container position-relative">
+            <div class="toast-container bottom-0 end-0"></div>
+        </div>
+      `;
+      document.body.insertAdjacentHTML("beforeend", notificationContainerHtml);
+    }
+
+    this.notificationContainer = container ?? document.querySelector(this.querySelector);
+
+    console.log(this.notificationContainer);
+    this.#getDiv(this.INFO);
+    this.#getDiv(this.SUCCESS);
+    this.#getDiv(this.ERROR);
+    this.#getDiv(this.WARNING);
+  }
+
+  #getDiv(level) {
+    const htm = this.html(level);
+    this.notificationContainer.insertAdjacentHTML("beforeend", htm);
+  }
+
+  #errorModal() {
+    return `
+      <div class="modal fade" id="${this.id}-errorNotificationModal" 
+          data-bs-backdrop="static" 
+          data-bs-keyboard="false" 
+          tabindex="-1" 
+          aria-labelledby="${this.id}-errorNotificationModalTitle" 
+          aria-hidden="true">
+          <div class="modal-dialog modal-dialog-centered">
+              <div class="modal-content" style="max-width: 650px; width: 650px">
+                  <div class="modal-header">
+                      <h1 class="modal-title fs-5" id="${this.id}-errorNotificationModalTitle"></h1>
+                      <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
+                  </div>
+                  <div class="modal-body">
+                      <pre id="${this.id}-errorNotificationModalMessage"></pre>
+                  </div>
+                  <div class="modal-footer">
+                      <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                  </div>
+              </div>
+          </div>
+      </div>
+    `;
+  }
+
+  html(level) {
+    let bgClass = "bg-info text-dark";
+    let autoHide = true;
+    let modalHtml = "";
+
+    if (level === "success") {
+      bgClass = "bg-success text-white";
+    } else if (level === "error") {
+      bgClass = "bg-danger text-white";
+      modalHtml = this.#errorModal();
+      autoHide = this.autoHideOnError === undefined ? false : this.autoHideOnError;
+    } else if (level === "warning") {
+      bgClass = "bg-warning text-dark";
+    }
+
+    return `
+            ${modalHtml}            
+            <div id="${this.id}-notification-${level}" 
+                class="toast align-items-center ${bgClass}" 
+                role="alert"
+                aria-live="assertive"
+                data-bs-autohide="${autoHide}"
+                aria-atomic="true" 
+                data-bs-delay="${this.disapearAfter}">
+                <div class="d-flex">
+                    <div id="${this.id}-toast-body-${level}" class="toast-body"></div>
+                    <button type="button" 
+                        class="btn-close btn-close-white me-2 m-auto" 
+                        data-bs-dismiss="toast" 
+                        aria-label="Close">
+                    </button>
+                </div>
+            </div>`;
+  }
+
+  #showToast(message, level = "info") {
+    console.debug("Showing toast message:", message);
+
+    let toastElement = document.getElementById(`${this.id}-notification-${level}`);
+    const toastBody = document.getElementById(`${this.id}-toast-body-${level}`);
+    toastBody.innerHTML = message;
+
+    const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastElement);
+    toastBootstrap.show();
+  }
+
+  #showErrorModal(title, message) {
+    console.error("Showing error modal:", message);
+    const modalTitle = document.getElementById(`${this.id}-errorNotificationModalTitle`);
+    const modalBody = document.getElementById(`${this.id}-errorNotificationModalMessage`);
+
+    const toastBody = document.getElementById(`${this.id}-toast-body-error`);
+
+    toastBody.innerHTML += `
+            <span type="button"
+                data-bs-toggle="modal"       
+                data-bs-target="#${this.id}-errorNotificationModal"> 
+                    <u>show</u>
+            </span>`;
+
+    modalTitle.innerHTML = title;
+    modalBody.innerHTML = message === "string" ? message : JSON.stringify(message, null, 2);
+  }
+
+  error(message, title, details) {
+    console.error("Error toast:", message);
+    this.#showToast(message, this.ERROR);
+
+    if (title && details) {
+      this.#showErrorModal(title, details);
+    }
+  }
+
+  success(message) {
+    console.debug("Success toast:", message);
+    this.#showToast(message, this.SUCCESS);
+  }
+  info(message) {
+    console.debug("Info toast:", message);
+    this.#showToast(message, this.INFO);
+  }
+  warning(message) {
+    console.debug("Warning toast:", message);
+    this.#showToast(message, this.WARNING);
+  }
+}
+
 /**
  * Component for monitoring and displaying network status (Online/Offline).
  * Can also trigger Bootstrap Toast notifications on status change.
@@ -34,6 +190,7 @@ class Online extends core.EmitterComponent {
     this.notificationContainerId = this.containerID + "-online-container";
     this.notificationBodyId = `${this.notificationContainerId}-body`;
 
+    this.notificationInstance = null;
     this.element = null;
   }
 
@@ -69,27 +226,13 @@ class Online extends core.EmitterComponent {
     this.element = this.container;
 
     if (this.notify) {
-      let notificationTemplate = `
-            <div class="toast-container position-fixed bottom-0 end-0 p-3 me-2 mb-3">
-                <div id="${this.notificationContainerId}" 
-                    class="toast align-items-center alert alert-info p-0" 
-                    role="alert" 
-                    aria-live="assertive"
-                    aria-atomic="true" 
-                    data-bs-delay="${this.disapearAfter}">
-                    <div class="d-flex">
-                        <div id="${this.notificationBodyId}" class="toast-body text-white"></div>
-                        <button type="button" 
-                            class="btn-close btn-close-white me-2 m-auto"
-                            data-bs-dismiss="toast" 
-                            aria-label="Close">
-                        </button>
-                    </div>
-                </div>
-            </div>`;
-      const div = document.createElement("div");
-      div.insertAdjacentHTML("beforeend", notificationTemplate);
-      document.body.appendChild(div);
+      this.notificationInstance = new Notification({
+        name: this.name + "-toast",
+        options: {
+          autohide_on_error: true,
+          disappear_after: this.disapearAfter,
+        },
+      });
     }
     window.addEventListener("online", this.updateStatus.bind(this));
     window.addEventListener("offline", this.updateStatus.bind(this));
@@ -100,15 +243,12 @@ class Online extends core.EmitterComponent {
    */
   updateStatus() {
     const status = this.isOnline();
-    if (this.notify && this.isCreated()) {
-      document.getElementById(this.notificationBodyId).innerHTML = status
-        ? this.onlineNotificationText
-        : this.offlineNotificationText;
-      const toast = document.getElementById(this.notificationContainerId);
-      toast.classList.remove("bg-success", "bg-danger");
-      toast.classList.add(status ? "bg-success" : "bg-danger");
-      const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toast);
-      toastBootstrap.show();
+    if (this.notificationInstance && this.isCreated()) {
+      if (status) {
+        this.notificationInstance.success(this.onlineNotificationText);
+      } else {
+        this.notificationInstance.error(this.offlineNotificationText);
+      }
     }
     this.element.innerHTML = this.html();
   }
